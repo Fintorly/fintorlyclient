4c815d648c4548d9482c24585ced2ce97832f7b0
Furkan <furkanvsarda@gmail.com>
2022-12-03 03:44:09 +0300

    pod file update

---
+++ src/Router.tsx
@@ -0,0 +1,20 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Router = (props: Props) => {
+    return (
+        <SafeAreaView>
+            <Text
+                style={{
+                    color: 'white',
+                }}
+            >lavuk youneeeeesss</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Router
+
+const styles = StyleSheet.create({})
